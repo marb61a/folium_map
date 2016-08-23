ea438a69279c2ebd8279d83d4652403c41bfdf8c
martin byrne <marb61a@gmail.com>
2016-08-23 11:06:26 +0000

    added different colour markers

--- script1.py
+++ script1.py
@@ -5,8 +5,8 @@ df=pandas.read_csv("Volcanoes-USA.txt")
 
 map=folium.Map(location=[45.372,-121.697],zoom_start=10,tiles='Stamen Terrain')
 
-for lat,lon,name in zip(df['LAT'],df['LON'],df['NAME']):
-    map.simple_marker(location=[lat,lon],popup=name,marker_color='red')
+for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
+    map.simple_marker(location=[lat,lon],popup=name,marker_color='green' if elev in range(0,1000) else 'orange' if elev in range(1000,3000) else 'red')
     
 
 
